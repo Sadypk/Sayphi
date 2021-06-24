2e3d05ce5cbf598f36f385570126a33ba6967e61
Taz <h.zzzaman@gmail>
2021-06-24 17:07:33 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:sayphi/app.dart';
 import 'package:sayphi/mainApp/util/env.dart';
+import 'package:sayphi/mainApp/util/fcmHandler.dart';
 import 'package:sayphi/mainApp/util/localStorage.dart';
 
 main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// firebase core
+  await Firebase.initializeApp();
+
+
+  /// init notification
+  await FCMHandler.init();
 
   /// loading environment variables
   await Env.init();
